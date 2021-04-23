6e7f359a06336a8986a5cd7588db322796917485
Omar Siam <simar@gmx.net>
2021-04-23 18:41:00 +0200

    Add parts that proved useful in herokuish envrionment.

--- dboeannotation/settings/base.py
+++ dboeannotation/settings/base.py
@@ -80,7 +80,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'allow_cidr.middleware.AllowCIDRMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
