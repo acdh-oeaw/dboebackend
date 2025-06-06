55853a5e23c15a6413031131e2867875ba9733f9
csae8092 <p.andorfer@gmail.com>
2025-06-06 15:10:52 +0200

    wip: adds custom save function to exract values from beleg xml and saves the values into dedicated model fields

--- belege/admin.py
+++ belege/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from belege.models import BelegSimple
+from belege.models import Beleg
+from django.db import models
 
 
-admin.site.register(BelegSimple)
+@admin.register(Beleg)
+class BelegAdmin(admin.ModelAdmin):
+    list_display = [
+        field.name
+        for field in Beleg._meta.fields
+        if isinstance(field, (models.CharField, models.TextField))
+    ]
+    search_fields = [
+        field.name
+        for field in Beleg._meta.fields
+        if isinstance(field, (models.CharField, models.TextField))
+    ]
---
+++ belege/management/commands/import.py
@@ -0,0 +1,30 @@
+import glob
+import lxml.etree as ET
+import os
+from tqdm import tqdm
+from django.core.management.base import BaseCommand
+from belege.models import Beleg
+
+from acdh_tei_pyutils.tei import TeiReader
+from acdh_tei_pyutils.utils import get_xmlid
+
+
+class Command(BaseCommand):
+    help = "imports dboe xmls"
+
+    def handle(self, *args, **options):
+        files = sorted(glob.glob("/home/csae8092/repos/dboe/legacy-data/orig-files/*xml"))
+
+        for x in files:
+            fname = os.path.split(x)[-1]
+            doc = TeiReader(x)
+            items = doc.any_xpath(".//tei:entry")
+            xenos = doc.any_xpath(".//tei:xenoData")
+            print(f"processing {len(items)} entries from {fname}")
+            for i, entry in tqdm(enumerate(items), total=len(items)):
+                xml_id = get_xmlid(entry)
+                node_as_text = ET.tostring(entry, encoding='unicode')
+                beleg, _ = Beleg.objects.get_or_create(dboe_id=xml_id)
+                beleg.orig_xml = node_as_text
+                beleg.xeno_data = xenos[i].text
+                beleg.save()
