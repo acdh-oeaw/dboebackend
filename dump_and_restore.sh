dcc14c2e751671b13539be0d7db86b727f66f361
csae8092 <p.andorfer@gmail.com>
2025-06-11 14:08:05 +0200

    chore: switched to dj-database-url

--- dump_and_restore.sh
+++ dump_and_restore.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-pg_dump -d dboeannotation_prod -h localhost -p 5433 -U  dboeannotation_prod -c -f dboeannotation_prod_dump.sql
-psql -U postgres -d dboeannotation_prod < dboeannotation_prod_dump.sql
+# pg_dump -d dboebackend -h localhost -p 5432 -U  postgres -c -f dboebackend_dump.sql
+
+
+# pg_dump -d dboeannotation_prod -h localhost -p 5433 -U  dboeannotation_prod -c -f dboeannotation_prod_dump.sql
+# psql -U postgres -d dboeannotation_prod < dboeannotation_prod_dump.sql
