d7210f8c4655c5e166901e7241cf4dd4afa17e97
csae8092 <p.andorfer@gmail.com>
2025-06-26 13:22:21 +0200

    fix: better filters

--- belege/api_utils.py
+++ belege/api_utils.py
@@ -14,33 +14,35 @@ CHAR_LOOKUP_CHOICES = [
 
 def get_filterset_for_model(model_class):
     """Returns a FilterSet class for the given model_class."""
+
     class DynamicFilterSet(filters.FilterSet):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
-            for field in self._meta.model._meta.fields:
+            for field in (
+                self._meta.model._meta.fields + self._meta.model._meta.many_to_many
+            ):
                 field_name = field.name
-                if isinstance(field, models.CharField) or isinstance(field, models.TextField):
-                    self.filters[f'{field_name}__icontains'] = filters.CharFilter(
+                if isinstance(
+                    field, (models.CharField, models.TextField, models.BooleanField)
+                ):
+                    self.filters[f"{field_name}__icontains"] = filters.CharFilter(
                         field_name=field_name,
-                        lookup_expr='icontains',
+                        lookup_expr="icontains",
                         label=f"{field.verbose_name} contains",
-                        help_text=field.help_text
+                        help_text=field.help_text,
                     )
-                elif isinstance(field, models.ForeignKey):
+                elif isinstance(field, (models.ForeignKey, models.ManyToManyField)):
                     self.filters[field_name] = filters.ModelChoiceFilter(
                         field_name=field_name,
                         queryset=field.related_model.objects.all(),
                         label=field.verbose_name,
                         help_text=f"Enter ID for {field.verbose_name}",
-                        widget=TextInput(attrs={
-                            'placeholder': 'Enter ID',
-                        })
+                        widget=TextInput(
+                            attrs={
+                                "placeholder": "Enter ID",
+                            }
+                        ),
                     )
-                # else:
-                #     try:
-                #         self.filters[field_name]["label"] = field.verbose_name
-                #     except KeyError:
-                #         pass
 
         class Meta:
             model = model_class
