0ec9c22cbae4b2fa4daac3a5e109be8a067c23d1
csae8092 <p.andorfer@gmail.com>
2025-10-08 12:12:43 +0200

    chore: linting

--- belege/management/commands/import.py
+++ belege/management/commands/import.py
@@ -1,19 +1,22 @@
 import glob
-import lxml.etree as ET
 import os
-from tqdm import tqdm
-from django.core.management.base import BaseCommand
-from belege.models import Beleg
 
+import lxml.etree as ET
 from acdh_tei_pyutils.tei import TeiReader
 from acdh_tei_pyutils.utils import get_xmlid
+from django.core.management.base import BaseCommand
+from tqdm import tqdm
+
+from belege.models import Beleg
 
 
 class Command(BaseCommand):
     help = "imports dboe xmls"
 
     def handle(self, *args, **options):
-        files = sorted(glob.glob("/home/csae8092/repos/dboe/legacy-data/orig-files/d178_*.xml"))
+        files = sorted(
+            glob.glob("/home/csae8092/repos/dboe/legacy-data/orig-files/d178_*.xml")
+        )
         for f, x in enumerate(files, start=1):
             print(f"{f}/{len(files)} files")
             fname = os.path.split(x)[-1]
@@ -23,7 +26,7 @@ class Command(BaseCommand):
             print(f"processing {len(items)} entries from {fname}")
             for i, entry in tqdm(enumerate(items), total=len(items)):
                 xml_id = get_xmlid(entry)
-                node_as_text = ET.tostring(entry, encoding='unicode')
+                node_as_text = ET.tostring(entry, encoding="unicode")
                 beleg, _ = Beleg.objects.get_or_create(dboe_id=xml_id)
                 beleg.orig_xml = node_as_text
                 try:
