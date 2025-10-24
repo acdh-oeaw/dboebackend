aacc32201a41a4257ede2ca1382656a4ab3a197c
csae8092 <p.andorfer@gmail.com>
2025-10-24 11:13:20 +0200

    wip feat: Beleg.save() (re)indexes Beleg object; ToDo: create bulk update workflow

---
+++ belege/opensearch_client.py
@@ -0,0 +1,27 @@
+import os
+
+from opensearchpy import OpenSearch
+
+host = "localhost"
+port = 9200
+auth = (
+    "admin",
+    os.environ.get("OS_PW", "Hansi4ever!"),
+)
+client = OpenSearch(
+    hosts=[{"host": host, "port": port}],
+    http_compress=True,
+    http_auth=auth,
+    use_ssl=True,
+    verify_certs=False,
+    ssl_assert_hostname=False,
+    ssl_show_warn=False,
+)
+try:
+    info = client.info()
+    print("Connection successful!")
+    print(f"OpenSearch version: {info['version']['number']}")
+    OS_CONNECTION = True
+except Exception as e:
+    print(f"Connection failed: {e}")
+    OS_CONNECTION = False
