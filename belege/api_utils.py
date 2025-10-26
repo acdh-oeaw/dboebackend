44d177877ad9c7f68f107635b31765890b2ba216
csae8092 <p.andorfer@gmail.com>
2025-10-26 08:30:05 +0100

    feat(api): add ids-endpoint

--- belege/api_utils.py
+++ belege/api_utils.py
@@ -1,8 +1,7 @@
-from django_filters import rest_framework as filters
-from django_filters import UnknownFieldBehavior
-from django.forms import TextInput
 from django.db import models
-
+from django.forms import TextInput
+from django_filters import UnknownFieldBehavior
+from django_filters import rest_framework as filters
 
 CHAR_LOOKUP_CHOICES = [
     ("icontains", "Contains"),
@@ -12,12 +11,25 @@ CHAR_LOOKUP_CHOICES = [
 ]
 
 
+def filter_by_ids(queryset, name, value):
+    values = value.split(",")
+    return queryset.filter(dboe_id__in=values)
+
+
 def get_filterset_for_model(model_class):
     """Returns a FilterSet class for the given model_class."""
 
     class DynamicFilterSet(filters.FilterSet):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
+
+            if hasattr(self._meta.model, "dboe_id"):
+                self.filters["ids"] = filters.CharFilter(
+                    method=filter_by_ids,
+                    label="DBOE IDs",
+                    help_text="Enter comma-separated DBOE IDs",
+                )
+
             for field in (
                 self._meta.model._meta.fields + self._meta.model._meta.many_to_many
             ):
