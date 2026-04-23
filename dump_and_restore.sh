83b3bd0e44aaa7848688a99aa4f072a87651bf47
csae8092 <p.andorfer@gmail.com>
2026-04-23 12:11:24 +0200

    chore: legacy-db

--- dump_and_restore.sh
+++ dump_and_restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# pg_dump -d dboebackend -h localhost -p 5433 -U  dboebackend -c -f dboebackend_dump.sql
-# psql -U postgres -d dboebackend < dboebackend_dump.sql
+pg_dump -d dboebackend -h localhost -p 5433 -U  dboebackend -c -f dboebackend_dump1.sql
+# psql -U postgres -d dboeplay < dboebackend_dump1.sql
 
 
 # pg_dump -d dboeannotation_prod -h localhost -p 5433 -U  dboeannotation_prod -c -f dboeannotation_prod_dump.sql
