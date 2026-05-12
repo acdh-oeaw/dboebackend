764765765ee516911d6106c5dd8efa3b4e778910
csae8092 <p.andorfer@gmail.com>
2026-05-12 09:22:44 +0200

    feat(data): adds import script for bibls

--- bibls/admin.py
+++ bibls/admin.py
@@ -1,3 +1,16 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from bibls.models import BibliographicItem, BibliographicType
+
+
+@admin.register(BibliographicType)
+class BibliographicTypeAdmin(admin.ModelAdmin):
+    list_display = ["main_type", "sub_type", "specification"]
+    list_filter = ["main_type"]
+
+
+@admin.register(BibliographicItem)
+class BibliographicItemAdmin(admin.ModelAdmin):
+    list_display = ["sigle", "short_title", "place", "bibl_type"]
+    search_fields = ["short_title"]
+    list_filter = ["bibl_type", "place"]
