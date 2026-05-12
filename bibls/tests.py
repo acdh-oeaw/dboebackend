4e24063b095a3cb25efa25cedc27c5a8fbfeb6d2
csae8092 <p.andorfer@gmail.com>
2026-05-12 17:48:52 +0200

    feat(tests): test bibl list views

--- bibls/tests.py
+++ bibls/tests.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from django.test import Client, TestCase
+from django.urls import reverse
 
 from bibls.models import BibliographicItem
+from dboeannotation.urls import router
 
 client = Client()
 
@@ -15,6 +17,10 @@ class BiblTestCase(TestCase):
         """Create test user"""
         User.objects.create_user(**USER)
 
+    def get_endpoints(self):
+        """Extract bibl endpoints from URL configuration"""
+        return [reverse(x.name) for x in router.urls if "list" in x.name]
+
     def test_001_save(self):
         items = BibliographicItem.objects.all()
         item = items.first()
@@ -22,3 +28,14 @@ class BiblTestCase(TestCase):
         item.zotero_key = "https://foo-bar/sumsi"
         item.save()
         self.assertTrue(item.in_zotero)
+
+    def test_002(self):
+        endpoints = [x for x in self.get_endpoints() if "bibliographic" in x]
+        for x in endpoints:
+            response = client.get(x)
+            self.assertEqual(
+                response.status_code,
+                200,
+                f"Expected 200 for {x}, got {response.status_code}",
+            )
+            self.assertIn("results", response.json())
