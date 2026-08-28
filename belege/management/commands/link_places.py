077c9f52460566e82423aa1631ef024b1058c65f
csae8092 <p.andorfer@gmail.com>
2026-08-28 12:03:27 +0200

    feat: updates link_places script * only processes Belege without links to siglen * only works on the `@corresp`

--- belege/management/commands/link_places.py
+++ belege/management/commands/link_places.py
@@ -1,10 +1,8 @@
 from django.core.management.base import BaseCommand
 from tqdm import tqdm
 
-from belege.models import (
-    Beleg,
-)
-from siglen.models import BelegSigle, Sigle
+from belege.models import Beleg, BelegSigle
+from siglen.models import Sigle
 
 namespaces = {"tei": "http://www.tei-c.org/ns/1.0"}
 
@@ -13,49 +11,40 @@ class Command(BaseCommand):
     help = "Links Belege to Siglen"
 
     def handle(self, *args, **options):
-        total = Beleg.objects.count()
-        for item in tqdm(Beleg.objects.iterator(), total=total):
+        queryset = Beleg.objects.filter(belegsigle__isnull=True).only(
+            "dboe_id", "orig_xml"
+        )
+        total = queryset.count()
+        # BelegSigle.objects.all().delete()
+        sigle_cache = {}
+        for item in tqdm(queryset.iterator(chunk_size=2000), total=total):
             try:
-                try:
-                    doc = item.orig_xml
-                except Exception as e:
-                    print(f"failed to parse {item.dboe_id} due to {e}")
-                    continue
-
-                for x in doc.xpath(".//tei:usg[@type='geo']", namespaces=namespaces):
-                    try:
-                        corresp = x.attrib["corresp"]
-                    except KeyError:
-                        corresp = None
-                    orig_names = x.xpath(
-                        ".//tei:placeName[@type='orig']/text()", namespaces=namespaces
-                    )
-                    try:
-                        sigle_str = (
-                            x.xpath(".//tei:listPlace/@corresp", namespaces=namespaces)[
-                                0
-                            ]
-                            .split("sigle:")[-1]
-                            .strip()
-                        )
-                    except IndexError:
-                        continue
-                    sigle_name = x.xpath(
-                        ".//tei:placeName/text()", namespaces=namespaces
-                    )[-1]
-                    try:
-                        sigle = Sigle.objects.get(sigle=sigle_str)
-                    except Sigle.DoesNotExist:
-                        sigle, _ = Sigle.objects.get_or_create(
-                            sigle=sigle_str, name=sigle_name, kind="ort"
-                        )
-                        print(f"created {sigle}")
-                    if orig_names is not None:
-                        sigle.orig_names = sigle.orig_names + orig_names
-                        sigle.save()
-                    BelegSigle.objects.get_or_create(
-                        beleg=item, sigle=sigle, corresp=corresp
-                    )
+                doc = item.orig_xml
             except Exception as e:
-                print(f"failed to process {item} due to {e}")
+                print(f"failed to parse {item.dboe_id} due to {e}")
+                continue
+
+            for x in doc.xpath(".//tei:usg[@type='geo']", namespaces=namespaces):
+                try:
+                    corresp = x.attrib["corresp"]
+                except KeyError:
+                    corresp = None
+
+                for full_sigle in x.xpath(
+                    ".//tei:listPlace/@corresp", namespaces=namespaces
+                ):
+                    if "sigle:" in full_sigle:
+                        sigle_str = full_sigle.split("sigle:")[-1]
+
+                        sigle = sigle_cache.get(sigle_str)
+                        if sigle is None:
+                            sigle, created = Sigle.objects.get_or_create(
+                                sigle=sigle_str,
+                            )
+                            if created:
+                                print(f"created {sigle}")
+                            sigle_cache[sigle_str] = sigle
+                        BelegSigle.objects.get_or_create(
+                            beleg=item, sigle=sigle, corresp=corresp
+                        )
         print("done")
